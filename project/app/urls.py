9734566f5b84215b5891f804a8b533f3bc2db7af
chetan <chetanagbattanwar26@gmail.com>
2024-02-08 18:50:17 +0530

    crud operation using class based APIView

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import showView,deleteView,updateView,createView
+from .views import showView,deleteView,updateView,createView #function based api_view
+from .views import studentGetPostAPIView,studentPutDeleteAPIView # class based APIView
 
 urlpatterns = [
     path('sv/',showView),
     path('cv/',createView),
     path('uv/<int:pk>/',updateView),
-    path('dv/<int:pk>/',deleteView)
+    path('dv/<int:pk>/',deleteView),
+    path('api/',studentGetPostAPIView.as_view()),
+    path('api/<int:pk>/',studentPutDeleteAPIView.as_view())
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,APIView
 from rest_framework.response import Response
 from .models import studentModel
 from .serializations import studentSerializer
@@ -34,3 +34,30 @@ def deleteView(request,pk):
     obj.delete()
     return Response(data={'message':'data deleted successfully'},status=status.HTTP_200_OK)
 
+#class based APIView
+class studentGetPostAPIView(APIView):
+    def get(self, request):
+        obj = studentModel.objects.all()
+        serializer = studentSerializer(obj, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = studentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class studentPutDeleteAPIView(APIView):
+    def put(self,request, pk):
+        obj = studentModel.objects.get(roll=pk)
+        serializer = studentSerializer(data=request.data, instance=obj, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        obj = studentModel.objects.get(roll=pk)
+        obj.delete()
+        return Response(data={'message': 'data deleted successfully'}, status=status.HTTP_200_OK)
