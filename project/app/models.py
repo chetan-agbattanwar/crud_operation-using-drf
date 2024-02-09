6a991898424445f2f5fe2d337f6b63d5b501c961
chetan <chetanagbattanwar26@gmail.com>
2024-02-09 12:14:25 +0530

    apply validators at marks fields in studentmodel class

--- project/app/models.py
+++ project/app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
 class studentModel(models.Model):
     roll = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=20)
-    marks = models.FloatField()
+    marks = models.FloatField( validators=[MinValueValidator(0),MaxValueValidator(100)])
